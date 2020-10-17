b789bb6a13bf6cbcaf05d31f2ba8025917ab36d3
arvindrenga96 <arvindrenga96@gmail.com>
2020-10-17 11:42:36 -0500

    create_instances
    To create and list insatnces and ip.

---
+++ create_instance.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import time
+import json
+
+import googleapiclient.discovery
+from six.moves import input
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "tensorflow-fresh-894033fbf80e.json"
+
+
+def list_instances(compute, project, zone):
+    # print(compute.instances.get)
+    result = compute.instances().list(project=project, zone=zone).execute()
+    ip = []
+    for i in range(len(result['items'])):
+        ip.append(result['items'][i]['networkInterfaces'][0]['networkIP'])
+    return result['items'] if 'items' in result else None, ip
+
+
+def create_instance(compute, project, zone, name, bucket):
+
+    image_response = compute.images().getFromFamily(
+        project='deeplearning-platform-release', family='tf-ent-latest-cpu').execute()
+    source_disk_image = image_response['selfLink']
+
+    # Configure the machine
+    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+    startup_script = open(
+        os.path.join(
+            os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+    #source_disk_image =
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                'key': 'bucket',
+                'value': bucket
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+
+
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+
+
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
+
+
+def main(project, bucket, zone, instance_name, wait=True):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    print('Creating instance.')
+
+    operation = create_instance(compute, project, zone, instance_name, bucket)
+    wait_for_operation(compute, project, zone, operation['name'])
+
+    instances, IP = list_instances(compute, project, zone)
+
+    print('Instances in project %s and zone %s:' % (project, zone))
+    for instance in instances:
+        print(' - ' + instance['name'])
+    print('Instances in project %s and zone %s: IPs are' % (project, zone))
+    print(IP)
+    print("""Instance created""".format(bucket))
+
+    if wait:
+        input()
+
+    print('Deleting instance.')
+
+    operation = delete_instance(compute, project, zone, instance_name)
+    wait_for_operation(compute, project, zone, operation['name'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id')
+    parser.add_argument(
+        'bucket_name')
+    parser.add_argument(
+        '--zone',
+        default='us-east1-c')
+    parser.add_argument(
+        '--name', default='demo-instance', help='New instance name.')
+
+    args = parser.parse_args()
+
+    main(args.project_id, args.bucket_name, args.zone, args.name)
---
+++ startup-script.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/peacekurella/tf-multiworker-demo.git
